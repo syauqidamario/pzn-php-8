e45eaca7661d792e129076919310bec88f187235
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-02-03 17:18:01 +0700

    Nullables in PHP8

---
+++ Nullable.php
@@ -0,0 +1,15 @@
+<?php
+
+class Address{
+    public ?string $country;
+}
+
+class User{
+    public ?Address $address;
+}
+
+function getCountry(?User $user): ?string{
+    return $user?->address?->country;
+}
+
+echo getCountry(null) . PHP_EOL;
