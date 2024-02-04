6c6852b76dea94bb8223d72e3dcbeef299a8ad84
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-02-04 11:51:44 +0700

    Comma parameter list

---
+++ CommaParameterList.php
@@ -0,0 +1,20 @@
+<?php
+
+function sayHi(string $first, string $last):void{
+
+}
+
+function sum(int... $values){
+
+}
+
+$array = [
+    "director1" => "Tomoki Sano",
+    "director2" => "Hirochika Muraishi",
+    "director3" => "Naoki Kohara",
+    "director4" => "Yuichi Kikuchi",
+    "director5" => "Tsugumi Kitaura",
+];
+
+sayHi("Masanao", "Akahoshi");
+sum(21,66,22);
